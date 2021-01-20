00a4cf9aa6ff4aa884c6e7e0e2c5079631998bba
julia_chizhik <for.yu.ch@gmail.com>
2021-01-20 16:08:57 +0200

    preFinal

--- src/App.js
+++ src/App.js
@@ -10,12 +10,27 @@ export class App extends Component {
 
   state = {
     contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+      // {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      // {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      // {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      // {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
     ],
     filter: '',
+  }
+
+  componentDidMount() { 
+   
+    if (localStorage.getItem('contacts') !== null) {
+      this.setState({
+        contacts: JSON.parse(localStorage.getItem('contacts'))
+      })
+    };
+  }
+
+  componentDidUpdate(prevProps, prevState) { 
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+     }
   }
 
   addContacts = (name,number) => { 
